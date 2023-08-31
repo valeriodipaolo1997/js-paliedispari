feea6b5c7565535f651f867f4328c6318f832b59
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-08-31 17:17:52 +0200

    add function for palindroma

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
 Dichiariamo chi ha vinto. */
 
-
+/*
 
 //definisco le variabili
 const userSelection = prompt("pari o dispari?");
@@ -40,3 +40,23 @@ function evenOrOdd(){
     
     //richiamo la funzione
     evenOrOdd()
+*/
+
+    /* Chiedere all’utente di inserire una parola 
+    Creare una funzione per capire se la parola inserita è palindroma */
+
+    
+    const parola = prompt("scegli una parola");
+
+const parolaInversa = invertiParola(parola);
+
+if(parola == parolaInversa){
+    console.log('la parola è palindroma');
+  } else {
+    console.log('la parola non è palindroma');
+  }
+  
+function invertiParola(str){
+  const strInversa = str.split('').reverse().join(''); 
+  return strInversa;
+}
